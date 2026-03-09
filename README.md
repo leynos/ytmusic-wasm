80cd6e60b3b534015b68e3d64528b21ffd1a6025
Payton McIntosh <pmcintosh@df12.net>
2026-03-09 00:02:22 +0000

    Add repo skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# YTMusic WASM Plugin for IronClaw
+
+This is a generated project using [Copier](https://copier.readthedocs.io/).
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! `YTMusic WASM Plugin for IronClaw` library.
+
+// TODO: Remove this stub and implement actual library functionality.
+/// Returns a greeting for the library.
+#[must_use]
+pub const fn greet() -> &'static str {
+    "Hello from YTMusic WASM Plugin for IronClaw!"
+}
